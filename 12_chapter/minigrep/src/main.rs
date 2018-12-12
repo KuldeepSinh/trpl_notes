796befc2d120dbf70b1fc99f0d4ed2aa0ff80c36
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-11 23:35:24 -0700

    Added ability of case insensitive search. Also added ability to send error to stderr.

--- 12_chapter/minigrep/src/main.rs
+++ 12_chapter/minigrep/src/main.rs
@@ -13,15 +13,14 @@ fn main() {
     //such as a vector, containing all the elements the iterator produces.
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments {}", err);
+        //eprintln prints to stderr
+        eprintln!("Problem parsing arguments {}", err);
         process::exit(1);
     });
 
-    //println!("Searching for {}.", &config.query);
-    //println!("In file {}.", &config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error {}.", e);
+        //eprintln prints to stderr
+        eprintln!("Application error {}.", e);
         process::exit(1);
     }
 }
