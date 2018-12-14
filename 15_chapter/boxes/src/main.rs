31a157972e8d4b72f1448bc39831ab60f54a2608
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-13 23:23:40 -0700

    Added examples for Deref.

--- 15_chapter/boxes/src/main.rs
+++ 15_chapter/boxes/src/main.rs
@@ -1,8 +1,8 @@
 //A pointer is a general concept for a variable that contains an address in memory.
 // The most common kind of pointer in Rust is a reference.
-//References are indicated by the & symbol and borrow the value they point to.
 
-//Smart pointers, on the other hand, are data structures that not only act like
+//"References" are indicated by the & symbol and borrow the value they point to.
+//"Smart pointers", on the other hand, are data structures that not only act like
 //a pointer but also have additional metadata and capabilities.
 
 //In Rust, an additional difference between references and smart pointers is that
@@ -11,27 +11,53 @@
 
 //Smart pointers are usually implemented using structs.
 //The characteristic that distinguishes a smart pointer from an ordinary struct is that
-//smart pointers implement the Deref and Drop traits.
+//smart pointers implement the "Deref" and "Drop" traits.
 //The "Deref" trait allows an instance of the smart pointer struct to behave like a
 //reference so you can write code that works with either references or smart pointers.
 //The "Drop" trait allows you to customize the code that is run when an instance of
 //the smart pointer goes out of scope.
 
-//The most straightforward smart pointer is a box, whose type is written Box<T>.
+//The most straightforward smart pointer is a Box, whose type is written Box<T>.
 //Boxes allow you to store data on the heap rather than the stack.
 
-#[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
 use crate::List::{Cons, Nil};
+use crate::NumList::{ICons, INil};
 
 fn main() {
+    //Prints 5.
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("{:?}", list);
+    //Prings the num-list
+    let numlist: NumList = ICons(1, Box::new(ICons(2, Box::new(ICons(3, Box::new(INil))))));
+    println!("{:?}", numlist);
+
+    let list: List<String> = Cons(
+        String::from("Hello"),
+        Box::new(Cons(
+            String::from("Wonderful"),
+            Box::new(Cons(String::from("World"), Box::new(Nil))),
+        )),
+    );
+    println!("Generic List {:?}", list);
+}
+
+//Creating a recursive type :
+//At compile time, Rust needs to know how much space a type takes up.
+//One type whose size can’t be known at compile time is a recursive type,
+//where a value can have as part of itself another value of the same type.
+//Because this nesting of values could theoretically continue infinitely,
+//Rust doesn’t know how much space a value of a recursive type needs.
+//However, boxes have a known size,
+//so by inserting a box in a recursive type definition, you can have recursive types.
+#[derive(Debug)]
+enum NumList {
+    ICons(i32, Box<NumList>),
+    INil,
+}
+
+#[derive(Debug)]
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil,
 }
---
+++ 15_chapter/derefs/src/lib.rs
@@ -0,0 +1,85 @@
+//Implementing the "Deref" trait allows you to customize the behavior of
+//the dereference operator, * (as opposed to the multiplication or glob operator).
+//By implementing "Deref" in such a way that a smart pointer can be treated
+//like a regular reference, you can write code that operates on references and
+//use that code with smart pointers too.
+#[cfg(test)]
+#[test]
+fn deref_test() {
+    let x = 42;
+    let y = &x; //y is a reference
+    let z = Box::new(x); //smart pointer
+
+    assert_eq!(42, x);
+    assert_eq!(42, *y); //dereference
+    assert_eq!(42, *z);
+
+    let y = MyBox::new(x);
+    //*y has been made possible by implementiong "Deref"
+    //When we entered *y in following Listing,
+    assert_eq!(42, *y); //behind the scenes Rust actually ran this code: *(y.deref())
+    assert_eq!(42, *(y.deref()));
+
+    //Example deref coercion (see fn hello)
+    let s = MyBox::new(String::from("hello"));
+    hello(&s);
+    //without deref coercion we would have to write as follows.
+    hello(&(*s)[..]);
+    //Anatomy of above line...
+    //The (*m) dereferences the MyBox<String> into a String.
+    //Then the & and [..] take a string slice of the String (.. means whole string)
+    //that is equal to the whole string to match the signature of hello
+}
+
+//Reminder: Following is a Tuple-Struct
+//and fields can be accessed using its ordinal position
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+impl<T> Deref for MyBox<T> {
+    //The "type Target = T;"
+    //syntax defines an "associated type" for the Deref trait to use.
+    type Target = T;
+
+    //We fill in the body of the deref method with &self.0
+    //so deref returns a reference to the value we want to access with the * operator.
+    //The deref method gives the compiler the ability to take a value of any type
+    //that implements Deref and call the deref method to get a & reference
+    //that it knows how to dereference.
+
+    //The reason the deref method returns a reference to a value,
+    //and that the plain dereference outside the parentheses in *(y.deref()) is still
+    //necessary, is the ownership system.
+    //If the deref method returned the value directly instead of a reference
+    //to the value, the value would be moved out of self.
+    //We don’t want to take ownership of the inner value inside MyBox<T> in this case
+    //or in most cases where we use the dereference operator.
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+//Deref Coercion:
+//Deref coercion is a convenience that Rust performs on arguments to functions and
+//methods.
+//Deref coercion converts a reference to a type that
+//implements Deref into a reference to a type
+//that Deref can convert the original type into.
+
+//Deref coercion happens automatically when we pass a reference to a particular type’s
+//value as an argument to a function or method that doesn’t match
+//the parameter type in the function or method definition.
+
+//A sequence of calls to the deref method converts the type we provided into
+//the type the parameter needs.
+
+//Example deref coercion
+pub fn hello(name: &str) {
+    assert_eq!("hello", name);
+}
